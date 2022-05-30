8ab46bedfcee5fca00a9e3a86898c71aa94674ed
Linsmar <linsmarvital@gmail.com>
2022-05-29 23:05:36 -0300

    inicia login

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,7 +1,7 @@
+import { prisma } from "./../../../lib/prisma";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import { prisma } from "../../../lib/prisma";
 
 export const authOptions = {
   providers: [
